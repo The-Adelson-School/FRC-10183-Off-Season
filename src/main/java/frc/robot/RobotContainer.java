d7d36b9a35e999e43dca835841c72409761fc684
Gavi Friedman <gavriel.friedman@theadelsonschool.org>
2025-09-03 12:13:23 -0700

    tetsYAY

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,8 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 // THIS SHOULD BE ON FRC OFF SEASON
-package frc.robot;
-
+package frc.robot; 
 import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -298,6 +297,7 @@ public Command reefTagIntakeStage1Command() {
   .andThen(new InstantCommand(() -> elevator.setIntakeSpeed(ElevatorConstants.INTAKE_IN)))
   .andThen(new InstantCommand(() -> elevator.setIntakeSpeed(ElevatorConstants.INTAKE_IN)));
 }
+System.out.println("Hello World");
 
 
 }
